07399c0c0a1e8f34885ea79f1936eb2c1c6404a7
Martin Gottweis <gottwik@gmail.com>
2017-02-27 08:42:13 +0100

    ad

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'gottwik',
+			tags: [],
+			salt: '1c004d03f7b5af188ea7c778b7cc186e',
+			hash: '49d19814a421e1df9bafa47b08ed6be34f3e641984e77742bc339c0616adca8d',
+			user_created_timestamp: 1488181228333
+		}
+	]
+}
